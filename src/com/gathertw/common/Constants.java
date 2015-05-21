91c6a98ceee0f63733624a2bc70c7552a70bfc1f
Pablo <pabloconcagran@gmail.com>
2015-05-21 20:37:12 +0200

    Added constants file

---
+++ src/com/gathertw/common/Constants.java
@@ -0,0 +1,11 @@
+package com.gathertw.common;
+
+public final class Constants {
+	
+	//Application authentication tokens
+	public static final String CONSUMER_KEY = "CONSUMER_KEY";
+	public static final String CONSUMER_SECRET = "CONSUMER_SECRET";
+	public static final String ACCESS_TOKEN = "ACCESS_TOKEN";
+	public static final String ACCESS_TOKEN_SECRET = "ACCESS_TOKEN_SECRET";
+	
+}
